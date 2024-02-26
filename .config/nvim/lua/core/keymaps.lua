e9d57bcc5be6a5b5e4363c920fc992e33d0dec9f
Mihai Caragheorghe <mihai.caragheorghe@multisoft.ro>
2024-02-26 11:29:14 +0200

    Initial commit

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,56 @@
+local keymap = vim.keymap
+
+vim.g.mapleader = " "
+
+-- Normal --
+-- General
+keymap.set("n", "#", "o<ESC>k") -- # for empty line below
+keymap.set("n", "<C-u>", "<C-u>zz") -- Ctrl + u will also center cursor
+keymap.set("i", "jj", "<ESC>:set rnu<CR>") -- jj instead of ESC
+keymap.set("n", "<C-d>", "<C-d>zz") -- Ctrl + d will also center cursor
+
+-- Window navigation
+keymap.set("n", "<C-l>", "<C-w>l") -- CTRL + l to move to right window
+keymap.set("n", "<C-k>", "<C-w>k") -- CTRL + k to move to above window
+keymap.set("n", "<C-j>", "<C-w>j") -- CTRL + j to move to below window
+keymap.set("n", "<C-h>", "<C-w>h") -- CTRL + h to move to left window
+
+-- Buffer navigation
+keymap.set("n", ")", ":bn<CR>") -- ) for next buffer
+keymap.set("n", "(", ":bp<CR>") -- ( for previous buffer
+keymap.set("n", "D", ":bd<CR>") -- D for deleting buffer
+
+-- Move text up and down
+keymap.set("n", "<A-j>", ":m .+1<CR>==")
+keymap.set("n", "<A-k>", ":m .-2<CR>==")
+keymap.set("n", "<A-Up>", ":m .-2<CR>==")
+keymap.set("n", "<A-Down>", ":m .+1<CR>==")
+
+-- Resize with arrows
+keymap.set("n", "<C-Up>", ":resize -2<CR>")
+keymap.set("n", "<C-Down>", ":resize +2<CR>")
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Visual --
+-- General
+keymap.set("v", "p", '"_dP') -- p will overwrite the selected text
+
+-- Stay in indent mode
+keymap.set("v", "<", "<gv^")
+keymap.set("v", ">", ">gv^")
+
+-- Move text up and down
+keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
+
+-- Visual Block --
+-- Move text up and down
+keymap.set("x", "J", ":m '>+1<CR>gv=gv")
+keymap.set("x", "K", ":m '<-2<CR>gv=gv")
+keymap.set("x", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap.set("x", "<A-k>", ":m '<-2<CR>gv=gv") 
+keymap.set("x", "<A-Down>", ":m '>+1<CR>gv=gv")
+keymap.set("x", "<A-Up>", ":m '<-2<CR>gv=gv") 
---
+++ .config/nvim/lua/core/plugins.lua
@@ -0,0 +1,52 @@
+local fn = vim.fn
+local cmd = vim.cmd
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer..."
+  cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- import packer safely
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- add list of plugins to install
+return packer.startup(function(use)
+	use "wbthomason/packer.nvim" -- Have packer manage itself
+	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+	use("tpope/vim-surround") -- surroundings
+
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
