4725bdfe4e1c1e5dd0a71822c38769f254853453
Mihai Caragheorghe <mihaicaragheorghe96@gmail.com>
2024-09-19 17:53:07 +0300

    Added alacritty config and switched to omnisharp

--- .config/nvim/lua/plugins/lazy/lspconfig.lua
+++ .config/nvim/lua/plugins/lazy/lspconfig.lua
@@ -174,7 +174,7 @@ return {
 			local servers = {
 				gopls = {},
 				pyright = {},
-				csharp_ls = {},
+				omnisharp = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
