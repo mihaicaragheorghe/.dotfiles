f196e003f4afaa8eed615962faa917fd728e733a
Mihai Caragheorghe <mihaicaragheorghe96@gmail.com>
2025-06-01 09:54:13 +0300

    Add. install scripts

---
+++ install/dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew install python
+brew install deno
+brew install go
---
+++ install/essentials.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+brew update
+brew install git
+brew install vim
+brew install fzf
+brew install tmux
+brew install stow
+brew install tree
+brew install ripgrep
